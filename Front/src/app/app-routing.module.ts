bc6a8c715231e0de5eee628cdcdd26a0bb2ad241
Alexscdt <49942651+alexscdt@users.noreply.github.com>
2021-10-16 08:31:46 +0200

    add component Angular/ add app.js back

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./component/home/home.component";
+import {GameComponent} from "./component/game/game.component";
+import {HistoriqueComponent} from "./component/historique/historique.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full'  },
+  { path: 'Home', component: HomeComponent, pathMatch: 'full'  },
+  { path: 'Game', component: GameComponent, pathMatch: 'full'  },
+  { path: 'History', component: HistoriqueComponent, pathMatch: 'full'  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
